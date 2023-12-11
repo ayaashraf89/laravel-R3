30f9bf48227ce05e24dc4b213f0595e5c80faf2e
ayaashraf89 <152074035+ayaashraf89@users.noreply.github.com>
2023-12-11 21:23:09 +0200

    Add files via upload

---
+++ Controllers/PostController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::get();
+        return view ('posts', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('addPost');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $posts = new Post();
+        $posts->posttitle = $request->posttitle;
+        $posts->description = $request->description;
+        $posts->author = $request->author;
+        if(isset($request->published)){
+            $posts->published = 1;
+        }else{
+            $posts->published = 0;
+        }
+        $posts->save();
+        return 'data added successfully';
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
